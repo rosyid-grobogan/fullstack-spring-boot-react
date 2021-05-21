b07710e3b29ca59455a7858c9f3ec4bbd66f607f
Rosyid <rosyidgrobogan@gmail.com>
2021-05-21 20:32:34 +0700

    + Testing error && setup error in .yml

--- src/main/java/com/rosyidgrobogan/fullstackspringbootreact/student/StudentController.java
+++ src/main/java/com/rosyidgrobogan/fullstackspringbootreact/student/StudentController.java
@@ -1,8 +1,6 @@
 package com.rosyidgrobogan.fullstackspringbootreact.student;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -14,23 +12,29 @@ public class StudentController {
 
     @GetMapping
     public List<Student> getAllStudents() {
-        return List.of(
-                new Student(
-                        UUID.randomUUID(),
-                        "Rosyid",
-                        "Grobogan",
-                        "rosyid@test.com",
-                        Student.Gender.MALE,
-                        Student.Status.ACTIVED),
+        throw new IllegalArgumentException("Oops cannot get all students");
+//        return List.of(
+//                new Student(
+//                        UUID.randomUUID(),
+//                        "Rosyid",
+//                        "Grobogan",
+//                        "rosyid@test.com",
+//                        Student.Gender.MALE,
+//                        Student.Status.ACTIVED),
+//
+//                new Student(
+//                        UUID.randomUUID(),
+//                        "Elisa",
+//                        "Tamara",
+//                        "putri@test.com",
+//                        Student.Gender.FEMALE,
+//                        Student.Status.UNACTIVED)
+//        );
+    }
 
-                new Student(
-                        UUID.randomUUID(),
-                        "Elisa",
-                        "Tamara",
-                        "putri@test.com",
-                        Student.Gender.FEMALE,
-                        Student.Status.UNACTIVED)
-        );
+    @PostMapping
+    public void addNewStudent(@RequestBody Student student) {
+        System.out.println(student);
     }
 
 }
